e411111909225097c4b96006a9bb9c0231f62752
一棵草 <960631249@qq.com>
2026-01-02 18:18:50 +0800

    11

--- service/service-pay/src/main/java/com/lanf/pay/PayApplication.java
+++ service/service-pay/src/main/java/com/lanf/pay/PayApplication.java
@@ -10,11 +10,10 @@ import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfigurat
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ImportResource;
 
 @MapperScan(basePackages = {"com.lanf.pay.mapper", SendMqMessageDO.MAN_SCAN_PACKAGE})
 @SpringBootApplication(scanBasePackages="com.lanf",exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-@ImportResource({"classpath:applicationContext.xml"})
+//@ImportResource({"classpath:applicationContext.xml"})
 
 @EnableDiscoveryClient  //nacos注册
 @EnableFeignClients(basePackages = {"com.lanf.log.api","com.lanf.finance.api","com.lanf.welfare.api"})
