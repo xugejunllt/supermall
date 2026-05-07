8900bcb342ebea3d3bfc4b6490432f6be82e1c70
一棵草 <960631249@qq.com>
2026-05-07 13:59:44 +0800

    11

---
+++ service/service-seckill/src/main/java/com/lanf/sms/SeckillApplication.java
@@ -0,0 +1,21 @@
+package com.lanf.sms;
+
+import com.lanf.messagemanager.client.model.SendMqMessageDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@MapperScan(basePackages = {"com.lanf.sms.mapper", SendMqMessageDO.MAN_SCAN_PACKAGE})
+@SpringBootApplication(scanBasePackages="com.lanf")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.lanf.log.api"})
+//nacos注册
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+
+}
