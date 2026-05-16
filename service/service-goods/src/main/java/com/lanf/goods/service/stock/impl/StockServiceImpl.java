4bb0c98e85c5ec67a64163bd3d8f1f5eea8ca274
一棵草 <960631249@qq.com>
2026-05-17 03:02:44 +0800

    11

--- service/service-goods/src/main/java/com/lanf/goods/service/stock/impl/StockServiceImpl.java
+++ service/service-goods/src/main/java/com/lanf/goods/service/stock/impl/StockServiceImpl.java
@@ -44,7 +44,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.*;
 
 /**
@@ -495,22 +494,7 @@ public class StockServiceImpl extends ServiceImpl<StockMapper, StockDO> implemen
         throw new BizException(GoodsCodeEnum.ADDRESS_EMPTY.getCode(), GoodsCodeEnum.ADDRESS_EMPTY.getMessage());
     }
     
-    private BigDecimal calculateDistance(BigDecimal lon1, BigDecimal lat1, 
-                                         BigDecimal lon2, BigDecimal lat2) {
-        double dLat = Math.toRadians(lat2.doubleValue() - lat1.doubleValue());
-        double dLon = Math.toRadians(lon2.doubleValue() - lon1.doubleValue());
-        
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                   Math.cos(Math.toRadians(lat1.doubleValue())) * 
-                   Math.cos(Math.toRadians(lat2.doubleValue())) *
-                   Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        
-        double distance = 6371 * c;
-        
-        return new BigDecimal(distance).setScale(2, RoundingMode.HALF_UP);
-    }
+
     
     @Override
     public PageResult<StockPageVO> stockPageQuery(UserStockPageQuery query) {
