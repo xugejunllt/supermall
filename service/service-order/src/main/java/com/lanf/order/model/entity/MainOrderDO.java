6f639a5eee81a2826dd7a12c07d40c5919136616
一棵草 <960631249@qq.com>
2026-05-19 17:12:33 +0800

    11

--- service/service-order/src/main/java/com/lanf/order/model/entity/MainOrderDO.java
+++ service/service-order/src/main/java/com/lanf/order/model/entity/MainOrderDO.java
@@ -1,5 +1,6 @@
 package com.lanf.order.model.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.lanf.mybatis.base.BaseEntity;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
  * @author jarven
  * @since 2026-02-27
  */
+@TableName("main_order")
 @Data
 public class MainOrderDO extends BaseEntity {
 
